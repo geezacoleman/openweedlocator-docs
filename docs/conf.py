346f7991516ba3efd639bc118b7773b85b2c8339
geezacoleman <guy@openag.tech>
2025-06-04 20:19:32 +0200

    initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 project = 'OpenWeedLocator'
-author = 'Guy Coleman, William Salter, and Contributors'
+author = 'Guy Coleman'
 copyright = f'{datetime.now().year}, {author}'
 release = '2.0.0'
 version = '2.0'
@@ -68,7 +68,7 @@ exclude_patterns = [
 ]
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'furo'  # Modern, clean theme
+html_theme = 'furo'
 html_title = 'OpenWeedLocator Documentation'
 html_short_title = 'OWL Docs'
 
@@ -93,7 +93,7 @@ html_theme_options = {
             'html': '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>',
         },
         {
-            'name': 'Scientific Reports Paper',
+            'name': 'Paper',
             'url': 'https://www.nature.com/articles/s41598-021-03858-9',
             'html': '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"><path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z"></path></svg>',
         },
